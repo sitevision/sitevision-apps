c31cf9ad2b2f01caee913499373e00ca092a2a15
Robin Lagren <robin.lagren@sitevision.se>
2019-12-23 12:13:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Create SiteVision app
+Create SiteVision Web/REST-apps
+
+## Getting started
+To create a new app, choose one of the following methods:
+
+### npx
+
+```sh
+npx create-sitevision-app my-new-app
+```
+
+([npx](https://medium.com/@maybekatz/introducing-npx-an-npm-package-runner-55f7d4bd282b) is a package runner tool (useful when you dont want to pollute global installations)
+
+__or__ install it globally using npm
+
+```sh
+npm install -g create-sitevision-app
+create-sitevision-app my-new-app
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+const create = require('./createSiteVisionApp').create;
+
+const currentNodeVersion = process.versions.node;
+const semver = currentNodeVersion.split('.');
+const major = semver[0];
+
+if (major < 10) {
+  console.error(
+    'You are running Node ' +
+      currentNodeVersion +
+      '.\n' +
+      'Create SiteVision App requires Node 10 or higher. \n' +
+      'Please update your version of Node.'
+  );
+  process.exit(1);
+}
+
+const appName = process.argv[2];
+const packagePath = process.argv[3];
+
+create(appName, packagePath);
