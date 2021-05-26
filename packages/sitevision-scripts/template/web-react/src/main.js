00beb47011b3c8d609fdd9644fbcb408bdf38df4
Albin Öhrn <albin.ohrn@sitevision.se>
2021-05-26 14:02:10 +0200

    COrrect parameter order

--- packages/sitevision-scripts/template/web-react/src/main.js
+++ packages/sitevision-scripts/template/web-react/src/main.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-export default (el, initialState) => {
+export default (initialState, el) => {
   ReactDOM.hydrate(
     <App message={initialState.message} name={initialState.name} />,
     el
