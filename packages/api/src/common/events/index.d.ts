154e72f21a2cd218e1fed706d24bf508ff2a248e
Jimmy Roselin <jimmy.roselin@sitevision.se>
2022-03-17 09:45:10 +0100

    fix: add doc for events

--- packages/api/src/common/events/index.d.ts
+++ packages/api/src/common/events/index.d.ts
@@ -1,5 +1,27 @@
+/**
+ * Listen to a given event
+ *
+ * @param eventName The name of the event
+ * @param callback The callback to trigger for the given event
+ */
 export function on(eventName: string, callback: (options: any) => void): void;
+/**
+ * Emit a server side event
+ *
+ * Note! Server side only
+ *
+ * @param eventName The name of the event to emit
+ * @param options The options for the event
+ */
 export function emit(eventName: string, options: any): void;
+/**
+ * Trigger a client side event
+ *
+ * Note! Client side only
+ *
+ * @param eventName The name of the event to trigger
+ * @param options The options for the event
+ */
 export function trigger(eventName: string, options: any): void;
 
 declare namespace events {
