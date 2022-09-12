f885434b1c42d0387037e459958e0e3988be3071
Albin Öhrn <albin.ohrn@sitevision.se>
2022-09-12 09:11:53 +0200

    fix(@sitevision/api): add security api documentation

---
+++ packages/api/src/common/security/index.d.ts
@@ -0,0 +1,26 @@
+export interface csrf {
+  /**
+   * Used to acquire the current csrf-token. If the user is anonymous or if csrf-protection is disabled on the server this will return null.
+   *
+   * @returns {string} the current token as string or null for anonymous users.
+   */
+  getToken(): string;
+  /**
+   * Used to acquire the parameter name that should be used when passing the token as a form field.
+   *
+   * @returns {string} the name that should be used when passing the token as form data.
+   */
+  getParameterName(): string;
+  /**
+   * Used to acquire the header name that should be used when passing the token as a request header.
+   *
+   * @returns {string} the name that should be used when passing the token as a header.
+   */
+  getHeaderName(): string;
+}
+
+export declare namespace security {
+  export { csrf };
+}
+
+export default security;
