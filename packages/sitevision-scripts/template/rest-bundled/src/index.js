a30f0f00e7cad2a8e8e7aad03b23854a995e2f1c
Albin Öhrn <albin.ohrn@sitevision.se>
2022-11-25 15:56:26 +0100

    Add template that was removed by mistake

---
+++ packages/sitevision-scripts/template/rest-bundled/src/index.js
@@ -0,0 +1,17 @@
+import router from '@sitevision/api/common/router';
+
+router.get('/myroute', (req, res) => {
+  res.json({ message: 'Hello from GET' });
+});
+
+router.post('/myroute', (req, res) => {
+  res.json({ message: 'Hello from POST' });
+});
+
+router.put('/myroute', (req, res) => {
+  res.json({ message: 'Hello from PUT' });
+});
+
+router['delete']('/myroute', (req, res) => {
+  res.json({ message: 'Hello from DELETE' });
+});
