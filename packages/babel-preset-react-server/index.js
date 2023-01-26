dc6152fa5715243c194f1efd61cb50c8e4b435e1
Filip Huhta <Filip.huhta@gmail.com>
2023-01-26 08:26:31 +0100

    fix(babel-preset-react-server): configure Babel to work with Typescript (#75)
    * configure Babel to work with Typescript
    
    * updated dependencies with @babel/preset-typescript
    
    Co-authored-by: Filip Huhta <filip.huhta@consid.se>

--- packages/babel-preset-react-server/index.js
+++ packages/babel-preset-react-server/index.js
@@ -1,5 +1,5 @@
 module.exports = function () {
   return {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
+    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
   };
 };
