8f4d6a332dc0b17aab4c0829a2333fe2cedd55f2
Albin Öhrn <albin.ohrn@sitevision.se>
2024-01-11 22:06:36 +0100

    Improve mocking interface for client side requester

--- packages/api/src/client/requester/index.js
+++ packages/api/src/client/requester/index.js
@@ -1,6 +1,27 @@
+/**
+ * Mocking interface for requester
+ * Bit of a hack, since it's really returns an $.ajax object.
+ * But $.ajax wraps a promise.
+ */
 export default {
-  doGet: () => {},
-  doPost: () => {},
-  doPut: () => {},
-  doDelete: () => {},
+  doGet: () => {
+    return new Promise((resolve) => {
+      resolve();
+    });
+  },
+  doPost: () => {
+    return new Promise((resolve) => {
+      resolve();
+    });
+  },
+  doPut: () => {
+    return new Promise((resolve) => {
+      resolve();
+    });
+  },
+  doDelete: () => {
+    return new Promise((resolve) => {
+      resolve();
+    });
+  },
 };
