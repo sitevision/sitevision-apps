55317291f185fb20bce5a89f8de08d1677481cf6
gustav-holmberg <99914613+gustav-holmberg@users.noreply.github.com>
2025-05-27 10:19:32 +0200

    feat(sitevision-scripts): Add option to activate executables deployed to prod
    Fixes #166
    
    `npm run deploy-prod -- --activate` to activate the deployed artifact

--- packages/sitevision-scripts/scripts/deploy-prod.js
+++ packages/sitevision-scripts/scripts/deploy-prod.js
@@ -6,8 +6,46 @@ import * as properties from '../util/properties.js';
 import chalk from 'chalk';
 import { getImportEndpoint, handleResponse } from './util/requests.js';
 import { getFullAppId } from './util/id.js';
+import minimist from 'minimist';
+
+const activateCustomModuleExecutable = async (
+  id,
+  baseUrl,
+  username,
+  password
+) => {
+  const activateUrl = `${baseUrl}/activateCustomModuleExecutable`;
+  try {
+    const activateResponse = await fetch(activateUrl, {
+      method: 'PUT',
+      body: JSON.stringify({
+        customModuleExecutableId: id,
+      }),
+      headers: {
+        Authorization: `Basic ${Buffer.from(username + ':' + password).toString(
+          'base64'
+        )}`,
+      },
+    });
+
+    handleResponse({
+      response: activateResponse,
+      operation: 'Activate Executable',
+    });
+  } catch (err) {
+    console.error(`${chalk.red('Activation failed, status code:')}, ${err}`);
+  }
+};
 
 (function () {
+  const args = minimist(process.argv, {
+    alias: { a: 'activate' },
+    default: {
+      activate: false,
+    },
+    boolean: ['activate'],
+  });
+
   let props;
   try {
     props = properties.getDevProperties();
@@ -84,14 +122,16 @@ import { getFullAppId } from './util/id.js';
         username = DEPLOY_USERNAME,
         password = DEPLOY_PASSWORD,
       }) => {
-        const url = `https://${domain}/rest-api/1/0/${encodeURIComponent(
+        const baseUrl = `https://${domain}/rest-api/1/0/${encodeURIComponent(
           siteName
-        )}/Addon%20Repository/${encodeURIComponent(addonName)}/${restEndPoint}`;
+        )}/Addon%20Repository/${encodeURIComponent(addonName)}`;
+
+        const deployUrl = `${baseUrl}/${restEndPoint}`;
         const formData = new FormData();
         formData.append('file', fs.createReadStream(zipPath));
 
         try {
-          const response = await fetch(url, {
+          const response = await fetch(deployUrl, {
             method: 'POST',
             body: formData,
             headers: formData.getHeaders({
@@ -101,7 +141,17 @@ import { getFullAppId } from './util/id.js';
             }),
           });
 
-          handleResponse({ response, operation: 'Upload' });
+          handleResponse({ response: response.clone(), operation: 'Upload' });
+          if (args.activate && response.ok) {
+            const { id } = await response.json();
+
+            await activateCustomModuleExecutable(
+              id,
+              baseUrl,
+              username,
+              password
+            );
+          }
         } catch (err) {
           console.log(`${chalk.red('Upload failed, status code:')} ${err}`);
         }
