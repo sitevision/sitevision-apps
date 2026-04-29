2b987bef137786a8b3f2d50dfb2ea2fd7a031ef7
Robin Lagren <robin.lagren@sitevision.se>
2026-04-29 15:37:43 +0200

    feat(sitevision-scripts): remove legacy app scaffolding

--- packages/sitevision-scripts/scripts/setup-dev-properties.js
+++ packages/sitevision-scripts/scripts/setup-dev-properties.js
@@ -4,32 +4,9 @@ import inquirer from 'inquirer';
 import * as properties from '../util/properties.js';
 import { questions } from '../config/setup-questions.js';
 
-const updatePackageJSON = (transpile) => {
-  const appPackage = properties.getPackageJson();
-  appPackage.sitevision_scripts_properties = {
-    transpile,
-  };
-  fs.writeFileSync(
-    properties.PACKAGE_JSON_PATH,
-    JSON.stringify(appPackage, null, 2)
-  );
-};
-
-const hasTranspileOptionInPackageJSON = () => {
-  const { sitevision_scripts_properties } = properties.getPackageJson();
-  return (
-    sitevision_scripts_properties &&
-    typeof sitevision_scripts_properties.transpile === 'boolean'
-  );
-};
-
 (function () {
-  const transpileOptionExistsInPackageJSON = hasTranspileOptionInPackageJSON();
   let existingDevProperties;
   let setupQuestions = questions.filter((q) => q.name !== 'type');
-  if (transpileOptionExistsInPackageJSON) {
-    setupQuestions = setupQuestions.filter((q) => q.name !== 'transpile');
-  }
 
   try {
     existingDevProperties = properties.getDevProperties();
@@ -48,22 +25,12 @@ const hasTranspileOptionInPackageJSON = () => {
 
       return question;
     });
-
-    if (
-      typeof existingDevProperties.transpile === 'boolean' &&
-      !transpileOptionExistsInPackageJSON
-    ) {
-      console.log(
-        'Transpile option will be moved from .dev-properties.json to package.json'
-      );
-    }
   }
 
   inquirer
     .prompt(setupQuestions)
     .then(
       ({
-        transpile,
         domain,
         siteName,
         addonName,
@@ -88,9 +55,6 @@ const hasTranspileOptionInPackageJSON = () => {
             2
           )
         );
-        if (!transpileOptionExistsInPackageJSON) {
-          updatePackageJSON(transpile);
-        }
       }
     );
 })();
