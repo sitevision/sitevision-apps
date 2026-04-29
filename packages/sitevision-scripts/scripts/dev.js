59de13202cfbb201478cf521b27ea8a0803101ee
Robin Lagren <robin.lagren@sitevision.se>
2026-04-29 15:38:15 +0200

    feat(sitevision-scripts): drop legacy app build support

--- packages/sitevision-scripts/scripts/dev.js
+++ packages/sitevision-scripts/scripts/dev.js
@@ -2,7 +2,6 @@ import path from 'path';
 import spawn from 'cross-spawn';
 import fs from 'fs-extra';
 import * as properties from '../util/properties.js';
-import resolveBin from 'resolve-bin';
 import webpack from 'webpack';
 import { copyChunksToResources } from './util/copychunks.js';
 import { getDirname } from '../util/dirname.js';
@@ -23,65 +22,53 @@ const SPAWN_PROPERTIES = {
 
 (async function () {
   const manifest = properties.getManifest();
-  if (manifest.bundled) {
-    const { default: webpackConfig } = await import(
-      '../config/webpack/webpack.config.js'
-    );
-
-    const appId = getFullAppId(manifest.id);
-    const compiler = webpack(
-      webpackConfig({
-        dev: true,
-        cssPrefix: appId,
-        serverApp: ['rest', 'mcpServer'].includes(properties.getAppType()),
-      })
-    );
-
-    compiler.watch(
-      {
-        ignored: ['**/dist/**', '**/build/**', '**/node_modules/**'],
-      },
-      (err, stats) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-
-        console.log(stats.toString({ colors: true }));
-        copyChunksToResources(properties.BUILD_DIR_PATH);
-
-        spawn.sync('node', [SITEVISION_SCRIPTS_PATH, 'zip'], SPAWN_PROPERTIES);
-        spawn.sync(
-          'node',
-          [SITEVISION_SCRIPTS_PATH, 'deploy', 'force'],
-          SPAWN_PROPERTIES
-        );
-      }
-    );
-
-    process.on('SIGINT', () => {
-      compiler.close((err) => {
-        if (err) {
-          console.error(err);
-        }
-
-        fs.existsSync(properties.BUILD_DIR_PATH) &&
-          fs.removeSync(properties.BUILD_DIR_PATH);
-      });
-    });
-  } else {
-    spawn(
-      resolveBin.sync('nodemon'),
-      [
-        '--watch',
-        properties.SRC_DIR_PATH,
-        '-e',
-        'js,html,css,less,json',
-        SITEVISION_SCRIPTS_PATH,
-        'build',
-        'force-deploy',
-      ],
-      SPAWN_PROPERTIES
-    );
+  if (!manifest.bundled) {
+    throw Error('Legacy apps are no longer supported by this script');
   }
+
+  const { default: webpackConfig } = await import(
+    '../config/webpack/webpack.config.js'
+  );
+
+  const appId = getFullAppId(manifest.id);
+  const compiler = webpack(
+    webpackConfig({
+      dev: true,
+      cssPrefix: appId,
+      serverApp: ['rest', 'mcpServer'].includes(properties.getAppType()),
+    })
+  );
+
+  compiler.watch(
+    {
+      ignored: ['**/dist/**', '**/build/**', '**/node_modules/**'],
+    },
+    (err, stats) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      console.log(stats.toString({ colors: true }));
+      copyChunksToResources(properties.BUILD_DIR_PATH);
+
+      spawn.sync('node', [SITEVISION_SCRIPTS_PATH, 'zip'], SPAWN_PROPERTIES);
+      spawn.sync(
+        'node',
+        [SITEVISION_SCRIPTS_PATH, 'deploy', 'force'],
+        SPAWN_PROPERTIES
+      );
+    }
+  );
+
+  process.on('SIGINT', () => {
+    compiler.close((err) => {
+      if (err) {
+        console.error(err);
+      }
+
+      fs.existsSync(properties.BUILD_DIR_PATH) &&
+        fs.removeSync(properties.BUILD_DIR_PATH);
+    });
+  });
 })();
